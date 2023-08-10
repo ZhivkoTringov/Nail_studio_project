1b4fa355da36ef9cd874d05378c36b82b22b94ca
Zhivko Tringov <zhivkotringov@gmail.com>
2023-08-10 17:04:55 +0300

    Deleting unnecessaryfiles.

--- appointments/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class Appointment(models.Model):
-
-
-    username = models.CharField(
-        max_length= 30,
-        null=False,
-        blank=False,
-    )
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from Nail_studio.core.views import IndexView, ServiceListView
-
-urlpatterns = (
- path('', IndexView.as_view(), name='index'),
-
-)
