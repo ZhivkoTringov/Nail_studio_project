1d01776ebfaa8fdaf33d0134b7496b643a564c63
Zhivko Tringov <zhivkotringov@gmail.com>
2023-09-24 23:28:12 +0300

    Giving a specific docstrings for models.py in the appointment app.

--- Nail_studio/appointments/models.py
+++ Nail_studio/appointments/models.py
@@ -8,6 +8,25 @@ UserModel = get_user_model()
 
 
 class Appointment(models.Model):
+    """
+      Represents an appointment for a manicure service.
+
+      Attributes:
+          time_ordered (DateTimeField): The date and time when the appointment was made.
+          manicurist (ForeignKey): The manicurist assigned to the appointment (linked to a User).
+          service (ForeignKey): The manicure service chosen for the appointment.
+          booked_by (CharField): The name of the user who booked the appointment.
+          start_time (DateTimeField): The start date and time of the appointment.
+          end_time (DateTimeField): The end date and time of the appointment.
+
+      Methods:
+          save(self, *args, **kwargs): Overrides the default save method to calculate and set
+              start_time and end_time if they are not provided.
+          calculate_start_time(self): Calculates the start time based on date and time fields.
+          is_time_slot_available(self): Checks if the appointment time slot is available and not
+              overlapping with other appointments.
+    """
+
     time_ordered = models.DateTimeField(
         default=datetime.now,
         blank=False
@@ -42,15 +61,31 @@ class Appointment(models.Model):
     )
 
     def save(self, *args, **kwargs):
+        """
+            Overrides the default save method to calculate and set start_time and end_time
+               if they are not provided.
+        """
+
         if not self.start_time or not self.end_time:
             self.start_time = self.calculate_start_time()
             self.end_time = self.start_time + timedelta(minutes=self.service.duration)
         super(Appointment, self).save(*args, **kwargs)
 
     def calculate_start_time(self):
+        """
+            Calculates the start time based on date and time fields.
+        """
+
         return datetime.combine(self.date, self.time)
 
     def is_time_slot_available(self):
+        """
+            Checks if the appointment time slot is available and not overlapping with other appointments.
+
+            Returns:
+                bool: True if the time slot is available; False if it's already booked.
+        """
+
         overlapping_appointments = Appointment.objects.filter(
             manicurist=self.manicurist,
             start_time__lt=self.end_time,
@@ -60,4 +95,8 @@ class Appointment(models.Model):
         return not overlapping_appointments.exists()
 
     def __str__(self):
+        """
+            Returns a string representation of the appointment.
+        """
+
         return f"Appointment: {self.start_time} - {self.end_time}, Manicurist: {self.manicurist}, Service: {self.service}"
