19ac38534dab6676cfed19753960f01d66f68014
Zhivko Tringov <zhivkotringov@gmail.com>
2023-09-26 20:43:58 +0300

    Adding aditional docstings.

--- Nail_studio/appointments/forms.py
+++ Nail_studio/appointments/forms.py
@@ -59,6 +59,13 @@ class AppointmentForm(forms.Form):
         self.fields['time'] = forms.ChoiceField(choices=self.get_available_times())
 
     def get_available_times(self):
+        """
+            Calculates and returns available appointment times.
+
+            Returns:
+                list: A list of available appointment times.
+        """
+
         start_time = datetime.strptime('10:00', '%H:%M').time()
         end_time = datetime.strptime('18:00', '%H:%M').time()
         TIME_SLOT = timedelta(minutes=15)
@@ -72,6 +79,13 @@ class AppointmentForm(forms.Form):
         return available_times
 
     def clean(self):
+        """
+            Validates and cleans the form data, ensuring a valid appointment.
+
+            Returns:
+                dict: A dictionary of cleaned and validated form data.
+        """
+
         cleaned_data = super().clean()
         chosen_date = cleaned_data.get('date')
         chosen_time = cleaned_data.get('time')
@@ -84,6 +98,20 @@ class AppointmentForm(forms.Form):
 
 
 class EditAppointmentForm(forms.ModelForm):
+    """
+    A form for editing an existing appointment.
+
+    This form allows users to edit the details of an existing appointment, including
+    the manicurist, service, start time, and end time.
+
+    Fields:
+        manicurist (ModelChoiceField): Allows users to select a manicurist.
+        service (ModelChoiceField): Allows users to select a service.
+        start_time (DateTimeField): Allows users to modify the appointment start time.
+        end_time (DateTimeField): Allows users to modify the appointment end time.
+
+    """
+
     class Meta:
         model = Appointment
         fields = ['manicurist', 'service', 'start_time', 'end_time']
