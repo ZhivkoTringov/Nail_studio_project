d4f40c754807c0101111f657783e80f645cd1a90
ZhivkoTringov <zhivkotringov@gmail.com>
2024-02-08 15:14:25 +0200

    config change

--- Nail_studio/settings.py
+++ Nail_studio/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
 
-STATICFILES_ROOT = os.getenv('STATIC_ROOT')
+STATIC_ROOT = os.getenv('STATIC_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
